b498d8972b739fdac09c10ead5104d857ec0a1e3
nikhil-balne <balne.chandra@gmail.com>
2025-05-14 10:27:57 +0530

    Backend repo setup

---
+++ backend/sentimentally/src/test/java/com/main/sentimentally/SentimentallyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.sentimentally;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentimentallyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
