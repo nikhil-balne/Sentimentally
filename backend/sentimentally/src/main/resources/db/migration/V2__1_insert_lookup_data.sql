0f333d7ed28fbca3ce1007d5498f577221be3b2f
Naik Shubhani <naik.shubhani180601@gmail.com>
2025-05-15 02:01:45 +0530

    Create V2__1_insert_lookup_data.sql

---
+++ backend/sentimentally/src/main/resources/db/migration/V2__1_insert_lookup_data.sql
@@ -0,0 +1,16 @@
+INSERT INTO severity (id, name) VALUES
+                                    (1, 'High'),
+                                    (2, 'Medium'),
+                                    (3, 'Low');
+
+INSERT INTO category (id, name) VALUES
+                                    (1, 'Housekeeping'),
+                                    (2, 'Maintenance'),
+                                    (3, 'Hygiene'),
+                                    (4, 'Amenities'),
+                                    (5, 'Disturbances'),
+                                    (6, 'Billing'),
+                                    (7, 'Dining'),
+                                    (8, 'Front Desk'),
+                                    (9, 'Security'),
+                                    (10, 'Technology');
