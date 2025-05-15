340da56433beccc005e0da6549bd6df359578ba7
Naik Shubhani <naik.shubhani180601@gmail.com>
2025-05-15 10:48:10 +0530

    Update V3__1_alter_property.sql

--- backend/sentimentally/src/main/resources/db/migration/V3__1_alter_property.sql
+++ backend/sentimentally/src/main/resources/db/migration/V3__1_alter_property.sql
@@ -2,3 +2,6 @@ ALTER TABLE property
     ALTER COLUMN incident_summary DROP NOT NULL,
     ALTER COLUMN feedback_summary DROP NOT NULL,
     ALTER COLUMN feedback_rating DROP NOT NULL;
+
+ALTER TABLE property
+DROP COLUMN description;
